c97a209674fa3895f7e458cf96f7e36d47db3aee
Bill Calkins <bill@calkinsconsulting.com>
2021-04-10 08:40:49 -0500

    Finished up to VCs

--- 08-project-1/projects/starter/Collage/MainViewController.swift
+++ 08-project-1/projects/starter/Collage/MainViewController.swift
@@ -27,58 +27,84 @@
 /// THE SOFTWARE.
 
 import UIKit
+import Combine
 
 class MainViewController: UIViewController {
-  
-  // MARK: - Outlets
-
-  @IBOutlet weak var imagePreview: UIImageView! {
-    didSet {
-      imagePreview.layer.borderColor = UIColor.gray.cgColor
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var imagePreview: UIImageView! {
+        didSet {
+            imagePreview.layer.borderColor = UIColor.gray.cgColor
+        }
     }
-  }
-  @IBOutlet weak var buttonClear: UIButton!
-  @IBOutlet weak var buttonSave: UIButton!
-  @IBOutlet weak var itemAdd: UIBarButtonItem!
-
-  // MARK: - Private properties
-  
-
-  // MARK: - View controller
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    let collageSize = imagePreview.frame.size
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var itemAdd: UIBarButtonItem!
     
-  }
-  
-  private func updateUI(photos: [UIImage]) {
-    buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
-    buttonClear.isEnabled = photos.count > 0
-    itemAdd.isEnabled = photos.count < 6
-    title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
-  }
-  
-  // MARK: - Actions
-  
-  @IBAction func actionClear() {
+    // MARK: - Private properties
+    private var subscriptions = Set<AnyCancellable>()
+    private var images = CurrentValueSubject<[UIImage], Never>([])
     
-  }
-  
-  @IBAction func actionSave() {
-    guard let image = imagePreview.image else { return }
     
-  }
-  
-  @IBAction func actionAdd() {
+    // MARK: - View controller
     
-  }
-  
-  private func showMessage(_ title: String, description: String? = nil) {
-    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { alert in
-      self.dismiss(animated: true, completion: nil)
-    }))
-    present(alert, animated: true, completion: nil)
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let collageSize = imagePreview.frame.size
+        
+        //1
+        images
+            .handleEvents(receiveOutput: {[weak self] photos in
+                self?.updateUI(photos: photos)
+            })
+            .map {photos in //2
+                UIImage.collage(images: photos, size: collageSize)
+            }
+            .assign(to: \.image, on: imagePreview)//3
+            .store(in: &subscriptions)//4
+    }
+    
+    private func updateUI(photos: [UIImage]) {
+        buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+        buttonClear.isEnabled = photos.count > 0
+        itemAdd.isEnabled = photos.count < 6
+        title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func actionClear() {
+        images.send([])
+    }
+    
+    @IBAction func actionSave() {
+        guard let image = imagePreview.image else { return }
+        
+    }
+    
+    @IBAction func actionAdd() {
+//        let newImages = images.value + [UIImage(named: "IMG_1907.jpg")!]
+//        images.send(newImages)
+        let photos = storyboard!.instantiateViewController(identifier: "PhotosViewController") as! PhotosViewController
+        
+        let newPhotos = photos.selectedPhotos
+        newPhotos
+            .map({[unowned self] newImage in
+                return self.images.value + [newImage] //1
+            })
+            .assign(to: \.value, on: images) //2
+            .store(in: &subscriptions) //3
+        
+        navigationController!.pushViewController(photos, animated: true)
+        
+    }
+    
+    private func showMessage(_ title: String, description: String? = nil) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { alert in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        present(alert, animated: true, completion: nil)
+    }
 }
--- 08-project-1/projects/starter/Collage/PhotosViewController.swift
+++ 08-project-1/projects/starter/Collage/PhotosViewController.swift
@@ -28,98 +28,104 @@
 
 import UIKit
 import Photos
+import Combine
 
 class PhotosViewController: UICollectionViewController {
-  
-  // MARK: - Public properties
-  
-  
-  // MARK: - Private properties
     
-  private lazy var photos = PhotosViewController.loadPhotos()
-  private lazy var imageManager = PHCachingImageManager()
-  
-  private lazy var thumbnailSize: CGSize = {
-    let cellSize = (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize
-    return CGSize(width: cellSize.width * UIScreen.main.scale,
-                  height: cellSize.height * UIScreen.main.scale)
-  }()
-  
-  // MARK: - View controller
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    // MARK: - Public properties
+    var selectedPhotos: AnyPublisher<UIImage, Never> {
+        return selectedPhotosSubject.eraseToAnyPublisher()
+    }
     
-    // Check for Photos access authorization and reload the list if authorized.
-    PHPhotoLibrary.fetchAuthorizationStatus { [weak self] status in
-      if status {
-        self?.photos = PhotosViewController.loadPhotos()
+    // MARK: - Private properties
+    private let selectedPhotosSubject = PassthroughSubject<UIImage, Never>()
+    
+    private lazy var photos = PhotosViewController.loadPhotos()
+    private lazy var imageManager = PHCachingImageManager()
+    
+    private lazy var thumbnailSize: CGSize = {
+        let cellSize = (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize
+        return CGSize(width: cellSize.width * UIScreen.main.scale,
+                      height: cellSize.height * UIScreen.main.scale)
+    }()
+    
+    // MARK: - View controller
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        DispatchQueue.main.async {
-          self?.collectionView.reloadData()
+        // Check for Photos access authorization and reload the list if authorized.
+        PHPhotoLibrary.fetchAuthorizationStatus { [weak self] status in
+            if status {
+                self?.photos = PhotosViewController.loadPhotos()
+                
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+            }
         }
-      }
-    }
-  }
-  
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-  }
-    
-  // MARK: - UICollectionViewDataSource
-  
-  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return photos.count
-  }
-  
-  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let asset = photos.object(at: indexPath.item)
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
-
-    cell.representedAssetIdentifier = asset.localIdentifier
-    imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
-      if cell.representedAssetIdentifier == asset.localIdentifier {
-        cell.preview?.image = image
-      }
-    })
-    
-    return cell
-  }
-  
-  // MARK: - UICollectionViewDelegate
-  
-  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    let asset = photos.object(at: indexPath.item)
-    
-    if let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell {
-      cell.flash()
     }
     
-    imageManager.requestImage(for: asset, targetSize: view.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { [weak self] image, info in
-      guard let self = self,
-        let image = image,
-        let info = info
-        else { return }
-      
-      if let isThumbnail = info[PHImageResultIsDegradedKey as String] as? Bool, isThumbnail {
-        // Skip the thumbnail version of the asset
-        return
-      }
-      
-      // Send the selected photo
-      
-    })
-  }
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        selectedPhotosSubject.send(completion: .finished)
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let asset = photos.object(at: indexPath.item)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
+        
+        cell.representedAssetIdentifier = asset.localIdentifier
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                cell.preview?.image = image
+            }
+        })
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let asset = photos.object(at: indexPath.item)
+        
+        if let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell {
+            cell.flash()
+        }
+        
+        imageManager.requestImage(for: asset, targetSize: view.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { [weak self] image, info in
+            guard let self = self,
+                  let image = image,
+                  let info = info
+            else { return }
+            
+            if let isThumbnail = info[PHImageResultIsDegradedKey as String] as? Bool, isThumbnail {
+                // Skip the thumbnail version of the asset
+                return
+            }
+            
+            self.selectedPhotosSubject.send(image)
+            
+        })
+    }
+    
 }
 
 // MARK: - Fetch assets
 extension PhotosViewController {
-  
-  static func loadPhotos() -> PHFetchResult<PHAsset> {
-    let allPhotosOptions = PHFetchOptions()
-    allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-    return PHAsset.fetchAssets(with: allPhotosOptions)
-  }
-  
+    
+    static func loadPhotos() -> PHFetchResult<PHAsset> {
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        return PHAsset.fetchAssets(with: allPhotosOptions)
+    }
+    
 }
